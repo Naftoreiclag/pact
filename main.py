fe622a9e3704e95aee1bb4d35bf53cc906f2672a
James Fong <james.fong@berkeley.edu>
2020-04-19 11:50:04 -0700

    OpenGL working pipe to Tkinter

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import tkinter
+from PIL import ImageTk
+import render
 
-window = tkinter.Tk()
-window.title('hello world')
-window.mainloop()
+def main():
+	tk_root = tkinter.Tk()
+	tk_root.title('hello world')
 
-print('application closed')
+	# Needs to happen after tk init for some reason
+	glfw_win = render.opengl_context_init()
+	
+	tk_canvas = tkinter.Canvas(tk_root, width=500, height=500, bg='black')
+	tk_canvas.pack(expand=tkinter.YES, fill=tkinter.BOTH)
+	
+	renderer = render.Renderer()
+
+	def clicky():
+		image = renderer.render()
+
+		tkimage = ImageTk.PhotoImage(image)
+		
+		if False:
+			label = tkinter.Label(tk_root, image=tkimage)
+			label.james_image_reference = tkimage
+			label.pack()
+		
+		tk_canvas.j_image_ref = tkimage
+		tk_canvas.delete(tkinter.ALL)
+		tk_canvas.create_image(0, 0, image=tkimage)
+
+	button = tkinter.Button(tk_root, text='clicky', command=clicky)
+	button.pack()
+
+	tk_root.mainloop()
+	render.opengl_context_cleanup(glfw_win)
+
+	print('application closed')
+
+if __name__ == '__main__':
+	main()
 
---
+++ render.py
@@ -0,0 +1,122 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import glfw
+
+from PIL import Image
+
+import random
+
+class Renderer:
+	
+	def __init__(self):
+		self._gl_fb_canvas = None
+		self._make_buffers(500, 500)
+		
+	def __del__(self):
+		self._cleanup_buffers()
+
+	def _make_buffers(self, width, height):
+		if self._gl_fb_canvas is not None:
+			raise RuntimeError('Attempted to overwrite FBO!')
+			
+		self._buff_width = width
+		self._buff_height = height
+		
+		# Create handles
+		self._gl_fb_canvas = glGenFramebuffers(1)
+		self._gl_rb_color = glGenRenderbuffers(1)
+		self._gl_rb_depth = glGenRenderbuffers(1)
+
+		# Select our FBO
+		glBindFramebuffer(GL_FRAMEBUFFER, self._gl_fb_canvas)
+
+		# Attach and initialize the color buffer
+		glBindRenderbuffer(GL_RENDERBUFFER, self._gl_rb_color)
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, self._buff_width, self._buff_height) # Important, use 8-bit depth for Pillow
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, self._gl_rb_color)
+
+		# Attach and initialize the depth buffer
+		glBindRenderbuffer(GL_RENDERBUFFER, self._gl_rb_depth)
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, self._buff_width, self._buff_height)
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, self._gl_rb_depth)
+		
+	def _cleanup_buffers(self):
+		# Delete everything in opposite order
+		glDeleteRenderbuffers(1, self._gl_rb_depth)
+		glDeleteRenderbuffers(1, self._gl_rb_color)
+		glDeleteFramebuffers(1, self._gl_fb_canvas)
+		
+		self._gl_fb_canvas = None
+		self._gl_rb_color = None
+		self._gl_fb_canvas = None
+
+	def _download_buffer_contents(self):
+		if self._gl_fb_canvas is None:
+			raise RuntimeError('Attempted to download non-existent contents!')
+		
+		# Select our canvas
+		glBindFramebuffer(GL_FRAMEBUFFER, self._gl_fb_canvas)
+		
+		# Alignment needs to be set in order for Pillow to read properly
+		glPixelStorei(GL_PACK_ALIGNMENT, 1)
+		
+		# Load data and convert to Pillow image
+		glReadBuffer(GL_COLOR_ATTACHMENT0)
+		raw_data = glReadPixels(0, 0, self._buff_width, self._buff_height, GL_RGBA, GL_UNSIGNED_BYTE)
+		image = Image.frombytes('RGBA', (self._buff_width, self._buff_height), raw_data)
+		
+		return image
+
+	def render(self):
+		# Select our canvas
+		glBindFramebuffer(GL_FRAMEBUFFER, self._gl_fb_canvas)
+		
+		# Clear old contents
+		glClearColor(0.5, 0.6, 0.7, 1.0)
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		
+		# Set up perspective
+		gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
+		glViewport(0, 0, self._buff_width, self._buff_height)
+
+		# Immediate mode draw
+		glBegin(GL_TRIANGLES)
+		for i in range(10):
+			glColor(random.random(), random.random(), random.random())
+			glVertex2d(random.random(), random.random())
+			glVertex2d(random.random(), random.random())
+			glVertex2d(random.random(), random.random())
+		glEnd()
+		glFlush()
+
+		# Download from device and return
+		image = self._download_buffer_contents()
+		return image
+
+def opengl_context_init():
+	# Try init
+	success = glfw.init()
+	if not success:
+		raise RuntimeError('GLFW init() failed!')
+	
+	# Not really important
+	window_width = 100
+	window_height = 100
+	
+	# This creates the OpenGL context and hidden window
+	glfw.window_hint(glfw.VISIBLE, False)
+	glfw_win = glfw.create_window(window_width, window_height, 'offscreen rendering window', None, None)
+	
+	# Check for errors
+	if not glfw_win:
+		glfw.terminate()
+		raise RuntimeError('Failed to create GLFW offscreen rendering window / OpenGL context!')
+
+	# Use context
+	glfw.make_context_current(glfw_win)
+	return glfw_win
+	
+def opengl_context_cleanup(glfw_win):
+	glfw.destroy_window(glfw_win)
+	glfw.terminate()
+
