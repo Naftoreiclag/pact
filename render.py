c373bcea80e84ec6110985af3db453e263fb4fc6
James Fong <james.fong@berkeley.edu>
2020-04-30 16:31:06 -0700

    Partially working perspective transform

--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 import moderngl
 import numpy as np
+import pyrr
 
 from PIL import Image
 
@@ -13,7 +14,9 @@ class Renderer:
 			vertex_shader='''
 				#version 330
 
-				in vec2 in_pos;
+				uniform mat4 unif_mvp;
+
+				in vec3 in_pos;
 				in vec3 in_color;
 
 				out vec3 vert_color;
@@ -21,8 +24,9 @@ class Renderer:
 
 				void main() {
 					vert_color = in_color;
-					vert_uv = in_pos;
-					gl_Position = vec4(in_pos, 0.0, 1.0);
+					vert_uv = in_pos.xy;
+					//gl_Position = vec4(in_pos, unif_mvp[0][0] * 0 + 1);
+					gl_Position = unif_mvp * vec4(in_pos, 1);
 				}
 			''',
 			fragment_shader='''
@@ -54,10 +58,22 @@ class Renderer:
 
 	def render(self):
 		
-		vert_buff = np.random.rand(300, 5)
+		vert_buff = np.random.rand(300, 6)
+		vert_buff[:,:3] = (np.round(vert_buff[:,:3]) * 2) - 1
 
+		mat_proj = pyrr.matrix44.create_perspective_projection_matrix(45.0, 1.0, 0.1, 100.0)
+		#mat_view = pyrr.matrix44.create_look_at((0, 0, 0), (0, 0, -1), (0, 1, 0))
+		#mat_view = pyrr.matrix44.create_look_at((10, 10, 10), (0, 0, 0), (0, 1, 0))
+		mat_mvp = mat_proj# @ mat_view
+		
+		print(mat_proj)
+		#mat_mvp = pyrr.matrix44.create_identity()
+		
 		vbo = self.ctx.buffer(vert_buff.astype(np.float32).tobytes())
 		vao = self.ctx.simple_vertex_array(self.shader_program, vbo, 'in_pos', 'in_color')
+
+
+		self.shader_program['unif_mvp'].write(mat_mvp.astype(np.float32).tobytes())
 
 		self.fbo.use()
 		self.texture.use()
