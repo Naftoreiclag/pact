0a727f1da009a44c9a98711bc79d5cba4354639b
James Fong <james.fong@berkeley.edu>
2020-05-04 22:00:44 -0700

    Drawing lines correctly

--- calibration.py
+++ calibration.py
@@ -11,8 +11,8 @@ class Calibrated_Image:
 class Control_Line:
 	
 	def __init__(self, start_pos, end_pos):
-		self.start_pos = start_pos
-		self.end_pos = end_pos
+		self.start_pos = np.array(start_pos)
+		self.end_pos = np.array(end_pos)
 
 class Calibration:
 	
@@ -22,8 +22,10 @@ class Calibration:
 		self.zoom_level = 1
 		self.cal_image = Calibrated_Image(Image.open('ignore/bears.jpg'))
 		
+		self.control_lines.append(Control_Line([0, 0], [4000, 3000]))
+		
 		self.tk_canvas = tk_canvas
-		self._load_tk_image([500, 500])
+		self._load_tk_image(0.1)
 		self.setup_binds()
 		
 	def setup_binds(self):
@@ -38,14 +40,25 @@ class Calibration:
 		
 		self.tk_canvas.delete('all')
 		
-		image_draw = origin_point - (self.tk_image_size / 2)
+		image_draw_point = origin_point - (self.tk_image_size / 2)
 		
-		self.tk_canvas.create_image(image_draw[0], image_draw[1], image=self.tk_image, anchor='nw')
-		self.tk_canvas.create_line(0, 0, 100, 200)
+		self.tk_canvas.create_image(image_draw_point[0], image_draw_point[1], image=self.tk_image, anchor='nw')
 		
-	def _load_tk_image(self, size):
-		self.tk_image_size = np.array(size)
-		width, height = size
+		for con_line in self.control_lines:
+			
+			start = image_draw_point + (con_line.start_pos * self.scale_factor)
+			end = image_draw_point + (con_line.end_pos * self.scale_factor)
+			
+			
+			
+			self.tk_canvas.create_line(start[0], start[1], end[0], end[1])
+		
+	def _load_tk_image(self, scale_factor):
+		image = self.cal_image.image
+		image_size = np.array((image.width, image.height))
+		self.scale_factor = scale_factor
+		self.tk_image_size = np.array(image_size * scale_factor).astype(np.int)
+		width, height = self.tk_image_size
 		self.tk_image = ImageTk.PhotoImage(self.cal_image.image.resize((width, height), Image.ANTIALIAS))
 	
 	def _on_canvas_press_m2(self, event):
