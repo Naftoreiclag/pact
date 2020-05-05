d94a92fcc90e00dad7ed0d8974f91849c82669f8
James Fong <james.fong@berkeley.edu>
2020-05-04 19:37:50 -0700

    Refactor Main: put state into class

--- main.py
+++ main.py
@@ -4,6 +4,49 @@ import render
 import time
 import numpy as np
 
+class Asdf():
+	def __init__(self, tk_canvas):
+		self.anchor_xy = np.zeros((2, ))
+		self.tk_canvas = tk_canvas
+		self.renderer = render.Renderer()
+		self.refresh = None
+		
+	def refresh_canvas(self):
+		image = self.renderer.render()
+		
+		tk_image = ImageTk.PhotoImage(image)
+		self.tk_canvas.usr_image_ref = tk_image
+		self.tk_canvas.delete('all')
+		self.tk_canvas.create_image(0, 0, image=tk_image, anchor='nw')
+	
+	def on_canvas_press_m1(self, event):
+		self.anchor_xy = np.array((event.x, event.y))
+		
+	def on_canvas_drag_m1(self, event):
+		new_anchor_xy = np.array((event.x, event.y))
+		
+		diff = new_anchor_xy - self.anchor_xy
+		self.renderer.view_params.yaw_rad -= (diff[0] / self.renderer.get_width()) * 2
+		self.renderer.view_params.pitch_rad += (diff[1] / self.renderer.get_height()) * 2
+		
+		self.renderer.view_params.pitch_rad = np.clip(self.renderer.view_params.pitch_rad, -np.pi/2, np.pi/2)
+		
+		self.anchor_xy = new_anchor_xy
+		self.refresh_canvas()
+	
+	
+	def on_canvas_reconfig(self,event):
+		width = event.width
+		height = event.height
+		
+		self.renderer.resize(width, height)
+		
+		self.refresh_canvas()
+		
+	def on_canvas_release_m1(self, event):
+		self.anchor_xy = None
+	
+
 def main():
 	tk_root = tkinter.Tk()
 	tk_root.title('hello world')
@@ -11,60 +54,20 @@ def main():
 	tk_canvas = tkinter.Canvas(tk_root, width=800, height=800, bg='black')
 	tk_canvas.pack(expand=True, fill='both')
 	
-	renderer = render.Renderer()
 	
-	def refresh_canvas():
-		image = renderer.render()
+	asdf = Asdf(tk_canvas)
 		
-		tk_image = ImageTk.PhotoImage(image)
-		tk_canvas.usr_image_ref = tk_image
-		tk_canvas.delete('all')
-		tk_canvas.create_image(0, 0, image=tk_image, anchor='nw')
-	
-	def on_canvas_reconfig(event):
-		width = event.width
-		height = event.height
-		
-		renderer.resize(width, height)
-		
-		refresh_canvas()
-		
-	anchor_xy = np.zeros((2, ))
-	def on_canvas_press_m1(event):
-		nonlocal anchor_xy
-		#print('Press', event.x, event.y)
-		anchor_xy = np.array((event.x, event.y))
-		
-	def on_canvas_drag_m1(event):
-		nonlocal anchor_xy
-		#print('Drag', event.x, event.y)
-		new_anchor_xy = np.array((event.x, event.y))
-		
-		diff = new_anchor_xy - anchor_xy
-		renderer.view_params.yaw_rad -= (diff[0] / renderer.get_width()) * 2
-		renderer.view_params.pitch_rad += (diff[1] / renderer.get_height()) * 2
-		
-		renderer.view_params.pitch_rad = np.clip(renderer.view_params.pitch_rad, -np.pi/2, np.pi/2)
-		
-		anchor_xy = new_anchor_xy
-		refresh_canvas()
-		
-	def on_canvas_release_m1(event):
-		nonlocal anchor_xy
-		#print('Release', event.x, event.y)
-		anchor_xy = None
-		
-	tk_canvas.bind('<Configure>', on_canvas_reconfig)
-	tk_canvas.bind('<ButtonPress-1>', on_canvas_press_m1)
-	tk_canvas.bind('<B1-Motion>', on_canvas_drag_m1)
-	tk_canvas.bind('<ButtonRelease-1>', on_canvas_release_m1)
+	tk_canvas.bind('<Configure>', asdf.on_canvas_reconfig)
+	tk_canvas.bind('<ButtonPress-1>', asdf.on_canvas_press_m1)
+	tk_canvas.bind('<B1-Motion>', asdf.on_canvas_drag_m1)
+	tk_canvas.bind('<ButtonRelease-1>', asdf.on_canvas_release_m1)
 
 	def clicky_1():
 		renderer._debug_scalar *= 0.9
-		refresh_canvas()
+		asdf.refresh_canvas()
 	def clicky_2():
 		renderer._debug_scalar /= 0.9
-		refresh_canvas()
+		asdf.refresh_canvas()
 
 	def update_canvas():
 		start = time.time()
